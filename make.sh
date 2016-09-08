a14e8252b8647fd152af6a6aec514dfcd7d386ef
Riccardo Balbo <riccardo@forkforge.net>
2016-09-08 12:59:33 +0200

    First

---
+++ make.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+REPO_NAME="jmonkeyengine"
+BRANCH="master"
+ROOT_MAKESH_GITHUB_USER="riccardobl"
+
+rm -Rf root_tmp
+mkdir -p root_tmp
+wget  -q https://raw.githubusercontent.com/$ROOT_MAKESH_GITHUB_USER/jme3-bullet-builder/root/make.sh -O root_tmp/root.sh
+source root_tmp/root.sh
+
+main $@
