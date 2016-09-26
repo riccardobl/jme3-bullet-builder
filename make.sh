59ae2d704d682b34de629faa264fbfcd0ddea557
Riccardo Balbo <riccardo@forkforge.net>
2016-09-26 23:39:31 +0200

    Download jni headers

--- make.sh
+++ make.sh
@@ -5,6 +5,12 @@ ROOT_MAKESH_GITHUB_USER="riccardobl"
 
 rm -Rf root_tmp
 mkdir -p root_tmp
+
+rm -Rf win
+mkdir -p win
+wget https://raw.githubusercontent.com/riccardobl/jme3-bullet-builder/root/win/jni_md.h -O win/jni_md.h
+wget https://raw.githubusercontent.com/riccardobl/jme3-bullet-builder/root/win/jawt_md.h -O win/jawt_md.h
+
 wget  -q https://raw.githubusercontent.com/$ROOT_MAKESH_GITHUB_USER/jme3-bullet-builder/root/make.sh -O root_tmp/root.sh
 source root_tmp/root.sh
 
