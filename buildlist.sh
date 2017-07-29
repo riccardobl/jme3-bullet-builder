a8b49816e4cd64e9363642c38a8d5fb870536df3
Riccardo Balbo <riccardo@forkforge.net>
2017-07-29 16:14:44 +0200

    Test build only 3.1

--- buildlist.sh
+++ buildlist.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 #Name Branch DEBUG(1/0) Repo
 export BUILD_LIST=(
-    "jmonkeyengine-master,master,0,https://github.com/jMonkeyEngine/jmonkeyengine.git"
-    "jmonkeyengine-master,master,1,https://github.com/jMonkeyEngine/jmonkeyengine.git"
-    "riccardobl-frk,frk,0,https://github.com/riccardobl/jmonkeyengine.git"
-    "riccardobl-frk,frk,1,https://github.com/riccardobl/jmonkeyengine.git"
     "jmonkeyengine-3.1-stable,v3.1,0,https://github.com/jMonkeyEngine/jmonkeyengine.git"
     "jmonkeyengine-3.1-stable,v3.1,1,https://github.com/jMonkeyEngine/jmonkeyengine.git"
-    "dokthar-softbody,bullet_SoftBody,0,https://github.com/Dokthar/jmonkeyengine.git"
-    "dokthar-softbody,bullet_SoftBody,1,https://github.com/Dokthar/jmonkeyengine.git"
 )
